ae7351fbd62ee048c67f9b32a941524a4133922a
snimmagadda1 <saiguy@me.com>
2020-03-15 14:27:21 -0400

    First query & mutation w/ prisma & yoga

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info){ // We have the prisma db object in ctx
+        // TODO: Check if they are logged in 
 
-module.exports = mutations;
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+    // createDog: function(parent, args, ctx, info){
+    //     // Create a dog
+    //     console.log(args);
+    // }
+};
+
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,17 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+
+const Query = {
+    items: forwardTo('db'), // This works b/c it is the same name & signature as the prisma method
+    // async items(parent, args, ctx, info){
+    //     console.log("Getting items")
+    //     const items = await ctx.db.query.items();
+
+    //     return items;
+    // }
+    // dogs: function(parent, args, ctx, info){
+    //     return [{name: 'Snickers'}, {name: 'Sunny'}]
+    // }
+};
 
 module.exports = Query;
