cb92755ace5d4d88de2476366914e76a13bcbab4
snimmagadda1 <saiguy@me.com>
2020-11-21 10:07:01 -0600

    Cart skeleton

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import Cart from './Cart';
 
 // This is used for the route change visualization of loading
 // No HTML/css needed!
@@ -72,7 +73,7 @@ const Header = () => (
         <div className="sub-bar">
             <p>Search</p>
         </div>
-        <div>Cart</div>
+        <Cart></Cart>
     </StyledHeader>
 )
 
