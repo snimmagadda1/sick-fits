27e4f89f0648adaf8008c3af148d1c3bad1f9d48
snimmagadda1 <saiguy@me.com>
2020-11-21 11:10:44 -0600

    Add toggle for cart

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -2,38 +2,46 @@ import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Signout from "./Signout";
+import { Mutation } from "react-apollo";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 
 const Nav = () => (
-    <User>
-        {/* Double destructure of payload -> data -> me into top level var me */}
-        {({ data: { me } }) => (
-            <NavStyles>
-                <Link href="/items ">
-                    <a>Shop</a>
-                </Link>
-                {me && (
-                    <>
-                        <Link href="/sell">
-                            <a>Sell</a>
-                        </Link>
-                        <Link href="/orders ">
-                            <a>Orders</a>
-                        </Link>
-                        <Link href="/me">
-                            <a>Account</a>
-                        </Link>
-                        <Signout></Signout>
-                    </>
-                )}
-
-                {!me && (
-                    <Link href="/signup">
-                        <a>Signup</a>
-                    </Link>
-                )}
-            </NavStyles>
+  <User>
+    {/* Double destructure of payload -> data -> me into top level var me */}
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items ">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders ">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+            <Signout></Signout>
+            {/* With a few lines of code we can expose this toggle
+                functionality anywhere within the app... 
+                no prop drilling */}
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
+          </>
         )}
-    </User>
+
+        {!me && (
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,6 +1,7 @@
 import withApollo from 'next-with-apollo'; // TODO what does this do? Something w/ SSSR
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -12,6 +13,29 @@ function createClient({ headers }) {
         },
         headers,
       });
+    },
+    clientState : {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            // 1. read cartOpen from cache 
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY,
+            });
+            console.log('The value of cartOpen is ', cartOpen);
+
+            // 2. swap value 
+            const data = {
+              data: { cartOpen: !cartOpen }
+            }; 
+            cache.writeData(data);
+            return data;
+          }
+        },
+      },
+      defaults: {
+        cartOpen: true,
+      },
     },
   });
 }
