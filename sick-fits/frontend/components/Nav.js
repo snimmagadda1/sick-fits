9406fabad49753c0f3c756491227d31ef2508fbb
snimmagadda1 <saiguy@me.com>
2020-11-27 15:17:37 -0600

    fix for edge case when delete cart

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,6 +4,7 @@ import User from "./User";
 import Signout from "./Signout";
 import { Mutation } from "react-apollo";
 import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCount from "./CartCount";
 
 const Nav = () => (
   <User>
@@ -29,7 +30,7 @@ const Nav = () => (
                 functionality anywhere within the app... 
                 no prop drilling */}
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {(toggleCart) => <button onClick={toggleCart}>My Cart</button>}
+              {(toggleCart) => <button onClick={toggleCart}>My Cart <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount></button>}
             </Mutation>
           </>
         )}
